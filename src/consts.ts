e9397af3ed0dbd3950106b19dca42e0aa57f3b13
Varun Krishnan <varun@fonearena.net>
2025-05-13 18:47:22 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Tera Omnni Media";
+export const SITE_TITLE = "Tera Omni Media";
 export const SITE_DESCRIPTION = "Your strategic digital partner";
